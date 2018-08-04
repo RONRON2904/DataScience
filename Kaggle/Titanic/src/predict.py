e5202e820568b500413bcb4aad92ff7f3cf73df6
Maxence <ronzie2904@hotmail.fr>
2018-08-04 22:37:20 +0200

    Update Titanic Code

--- Kaggle/Titanic/src/predict.py
+++ Kaggle/Titanic/src/predict.py
@@ -22,6 +22,17 @@ def to_npy(csv_file):
         pid = int(line['PassengerId'])
         pclass = int(line['Pclass'])
         sex = 1 if line['Sex'] == 'male' else 0
+        name = line['Name']
+        if sex:
+            if 'Mr.' in name:
+                name = 1
+            else:
+                name = 0
+        elif sex == 0:
+            if 'Mrs.' in name:
+                name = 1
+            else:
+                name = 0
         age = -1 if np.isnan(line['Age']) else int(line['Age'])
         sibsp = int(line['SibSp'])
         if pclass == 1:
@@ -32,7 +43,7 @@ def to_npy(csv_file):
             p = 10.
         parch =int(line['Parch'])
         fare =  p if np.isnan(line['Fare']) else int(line['Fare'])
-        cabin = 1  if isinstance(line['Cabin'], basestring) else 0
+        cabin = 1  if isinstance(line['Cabin'], str) else 0
         if line['Embarked'] == 'S':
             embarked = 0
         elif line['Embarked'] == 'C':
@@ -40,24 +51,19 @@ def to_npy(csv_file):
         elif line['Embarked'] == 'Q':
             embarked = 2
 
-        X = np.append(X, [pid, pclass, sex, age, sibsp, parch, fare, cabin, embarked])
-    return X.reshape(df.shape[0], 9)
-
+        X = np.append(X, [pid, pclass, name, sex, age, sibsp, parch, fare, cabin, embarked])
+    return X.reshape(df.shape[0], 10)
 
 def predict(X, male_model, female_model):
-#    survived = model.predict(X[:, 1:])
-    features = [u'PassengerId', 
+    features = [u'PassengerId',
                 u'Survived']
     values = []
     for i in range(X.shape[0]):
-        survived = male_model.predict(X[i, [1, 4, 5, 6, 8]].reshape(1, -1)) if X[i, 2] == 1 else female_model.predict(X[i, [1, 4, 5, 6, 8]].reshape(1, -1)) 
+        survived = male_model.predict(X[i, [1, 2, 6, 7, 8, 9]].reshape(1, -1)) if X[i, 3] == 1 else female_model.predict(X[i, [1, 2, 6, 7, 8, 9]].reshape(1, -1))
         values.append([str(int(X[i, 0])), str(int(survived))])
     create_csv_file('submission.csv', features, values)
-    
+
 
 if __name__ == '__main__':
     X = to_npy('data/CSV_FILES/test.csv')
-    predict(X, pickle.load(open('data/Models/lgr_male.pkl', 'rb')), pickle.load(open('data/Models/lgr_female.pkl', 'rb')))
-        
-    
-    
+    predict(X, pickle.load(open('data/Models/lgr_male.pkl', 'rb')), pickle.load(open('data/Models/xgb_female.pkl', 'rb')))
