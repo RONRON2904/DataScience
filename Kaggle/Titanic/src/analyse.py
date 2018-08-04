16c3890526f65a7fc8ec5d521f761991f096de11
Maxence <ronzie2904@hotmail.fr>
2018-08-04 22:37:20 +0200

    Update Titanic Code

--- Kaggle/Titanic/src/analyse.py
+++ Kaggle/Titanic/src/analyse.py
@@ -1,30 +1,115 @@
 import pandas as pd
+import matplotlib.pyplot as plt
 
 """
-This stat has been computed in order to see the correlation between a known cabin 
+This stat has been computed in order to see the correlation between a known cabin
 for a person and the fact he survived or not.
 
 The purpose afterwards is to put a binary information about it into the X_train.npy file:
 1 if the cabin is known else 0
 
-Indeed, in the given csv files we can't put this information into a numpy array as it is a string or NaN   
+Indeed, in the given csv files we can't put this information into a numpy array as it is a string or NaN
 """
 def cabin_stats():
-    df = pd.read_csv('data/train.csv')
+    df = pd.read_csv('data/CSV_FILES/train.csv')
     survived = df.loc[df['Survived'] == 1]
     dead = df.loc[df['Survived'] == 0]
     survivedStr = 0
     deadStr = 0
-    
+
     for i in range(survived.shape[0]):
         line = survived.iloc[i]
-        if isinstance(line['Cabin'], basestring):
+        if isinstance(line['Cabin'], str):
             survivedStr += 1
-            
+
     for i in range(dead.shape[0]):
         line = dead.iloc[i]
-        if isinstance(line['Cabin'], basestring):
+        if isinstance(line['Cabin'], str):
             deadStr += 1
 
     return float(survivedStr)/survived.shape[0], float(deadStr)/dead.shape[0]
 
+def ticket_stats():
+    df = pd.read_csv('data/CSV_FILES/train.csv')
+    survived = df.loc[df['Survived'] == 1]
+    dead = df.loc[df['Survived'] == 0]
+
+    for i in range(survived.shape[0]):
+        line = survived.iloc[i]
+        print('Survived', line['Ticket'])
+
+    for i in range(dead.shape[0]):
+        line = dead.iloc[i]
+        print('Dead', line['Ticket'])
+
+"""
+Is there any correlation between a Mr. male / not Mr. male and the fact that he survived or not?
+Same for female with 'Mrs.' or not 'Mrs.' status ?
+This stats aims at showing whether it is worth taking this information into account or not.
+"""
+def name_stats():
+    df = pd.read_csv('data/CSV_FILES/train.csv')
+    survived_male = df.loc[(df['Survived'] == 1) & (df['Sex'] == 'male')]
+    survived_female = df.loc[(df['Survived'] == 1) & (df['Sex'] == 'female')]
+    dead_male = df.loc[(df['Survived'] == 0) & (df['Sex'] == 'male')]
+    dead_female = df.loc[(df['Survived'] == 0) & (df['Sex'] == 'female')]
+
+    mr_survived = 0
+    not_mr_survived = 0
+    mr_dead = 0
+    not_mr_dead = 0
+
+    mrs_survived = 0
+    not_mrs_survived = 0
+    mrs_dead = 0
+    not_mrs_dead = 0
+
+
+    for i in range(survived_male.shape[0]):
+        line = survived_male.iloc[i]
+        if 'Mr.' in line['Name']:
+            mr_survived += 1
+        else :
+            not_mr_survived += 1
+
+    for i in range(dead_male.shape[0]):
+        line = dead_male.iloc[i]
+        if 'Mr.' in line['Name']:
+            mr_dead += 1
+        else :
+            not_mr_dead += 1
+
+    for i in range(survived_female.shape[0]):
+        line = survived_female.iloc[i]
+        if 'Mrs.' in line['Name']:
+            mrs_survived += 1
+        else :
+            not_mrs_survived += 1
+
+    for i in range(dead_female.shape[0]):
+        line = dead_male.iloc[i]
+        if 'Mr.' in line['Name']:
+            mrs_dead += 1
+        else :
+            not_mrs_dead += 1
+
+    total_mr = mr_survived + mr_dead
+    total_not_mr = not_mr_survived + not_mr_dead
+    total_mrs = mrs_survived + mrs_dead
+    total_not_mrs = not_mrs_survived + not_mrs_dead
+
+    return mr_survived/total_mr, mr_dead/total_mr, not_mr_survived/total_not_mr, not_mr_dead/total_not_mr, mrs_survived/total_mrs, mrs_dead/total_mrs, not_mrs_survived/total_not_mrs, not_mrs_dead/total_not_mrs
+
+"""
+This Function plots the bar histogram to show how correlated is a feature with the target Survived  
+"""
+def plot_bar(feature):
+    df = pd.read_csv('data/CSV_FILES/train.csv')
+    survived = df.loc[df['Survived'] == 1][feature].value_counts()
+    dead = df.loc[df['Survived'] == 0][feature].value_counts()
+    df = pd.DataFrame([survived, dead])
+    df.index = ['Survived', 'Dead']
+    df.plot(kind='bar', stacked=True, figsize=(10, 5))
+    plt.show()
+
+#plot_bar('SibSp')
--- Kaggle/Titanic/src/model.py
+++ Kaggle/Titanic/src/model.py
@@ -1,21 +1,21 @@
 from sklearn.linear_model import LogisticRegression
 from sklearn.tree import DecisionTreeClassifier
-from sklearn.ensemble import RandomForestClassifier, AdaBoostClassifier
+from sklearn.ensemble import RandomForestClassifier, AdaBoostClassifier, VotingClassifier
 from sklearn.neural_network import MLPClassifier
 from sklearn.neighbors import KNeighborsClassifier
-from sklearn.svm import SVC
+from sklearn.svm import SVC, LinearSVC
 from sklearn.naive_bayes import GaussianNB
 from data_preprocessing import to_npy
 from sklearn.discriminant_analysis import QuadraticDiscriminantAnalysis
-
+from xgboost import XGBClassifier
 from sklearn.preprocessing import normalize, MinMaxScaler, minmax_scale, MaxAbsScaler, StandardScaler, RobustScaler, Normalizer, QuantileTransformer
 
 import numpy as np
 import pickle
 
-X_train = np.load('data/NPY_FILES/Xm_train.npy')[:, [0, 2, 3, 4, 6]]
+X_train = np.load('data/NPY_FILES/Xm_train.npy')[:, [0, 1, 4, 5, 6, 7]]
 y_train = np.load('data/NPY_FILES/ym_train.npy')
-X_val = np.load('data/NPY_FILES/Xm_val.npy')[:, [0, 2, 3, 4, 6]]
+X_val = np.load('data/NPY_FILES/Xm_val.npy')[:, [0, 1, 4, 5, 6, 7]]
 y_val = np.load('data/NPY_FILES/ym_val.npy')
 
 #Try some standardisation/normalization on the dataset
@@ -39,34 +39,43 @@ val_distributions = [X_val,
 
 n=0
 
+# Test various classifiers 
 
 lgr = LogisticRegression()
 lgr.fit(train_distributions[n], y_train)
 pickle.dump(lgr, open('data/Models/lgr_male.pkl', 'wb'))
 
-knc = KNeighborsClassifier()
+knc = KNeighborsClassifier(n_neighbors=10, weights='distance')
 knc.fit(train_distributions[n], y_train)
 
 dtc = DecisionTreeClassifier()
 dtc.fit(train_distributions[n], y_train)
 
-rf = RandomForestClassifier(n_estimators=500)
+rf = RandomForestClassifier(n_estimators=400)
 rf.fit(train_distributions[n], y_train)
 
 adb = AdaBoostClassifier(n_estimators=50, learning_rate=0.1)
 adb.fit(train_distributions[n], y_train)
 
-mlp = MLPClassifier(hidden_layer_sizes=500, activation='logistic', learning_rate='adaptive')
+mlp = MLPClassifier(hidden_layer_sizes=100, activation='logistic', learning_rate='adaptive')
 mlp.fit(train_distributions[n], y_train)
 
 svc = SVC()
 svc.fit(train_distributions[n], y_train)
+
+
+lsvc = LinearSVC(C=0.1)
+lsvc.fit(train_distributions[n], y_train)
 
 gnb = GaussianNB()
 gnb.fit(train_distributions[n], y_train)
 
 qda = QuadraticDiscriminantAnalysis()
 qda.fit(train_distributions[n], y_train)
+
+xgbc = XGBClassifier(n_estimators=110, learning_rate=0.08, max_depth=6)
+xgbc.fit(train_distributions[n], y_train)
+#pickle.dump(lgr, open('data/Models/xgb_female.pkl', 'wb'))
 
 print('Score LogisticRegression :', lgr.score(val_distributions[n], y_val))
 print('Score DecisionTreeClassifier :', dtc.score(val_distributions[n], y_val))
@@ -78,7 +87,7 @@ print('Score RandomForestClassifier :', rf.score(val_distributions[n], y_val))
 print('Score AdaBoostClassifier :', adb.score(val_distributions[n], y_val))
 print('Score MLPClassifier :', mlp.score(val_distributions[n], y_val))
 print('Score SVC :', svc.score(val_distributions[n], y_val))
+print('Score LinearSVC :', lsvc.score(val_distributions[n], y_val))
 print('Score GNB :', gnb.score(val_distributions[n], y_val))
 print('Score QuadraticDiscriminantAnalysis :', qda.score(val_distributions[n], y_val))
-
-
+print('Score XGBClassifier :', xgbc.score(val_distributions[n], y_val))
